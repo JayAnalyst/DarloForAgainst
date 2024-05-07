91f748f9b83367ea626585fe92184ee5dfbab222
JayAnalyst <75013747+JayAnalyst@users.noreply.github.com>
2024-05-07 13:40:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ symbol_map = {
     'head': 'square'
 }
 color_discrete_map = {'Goal': 'green', 'No Goal': 'red'}
+
+stype = ['All shots','From Corner','Counter Attack','From Cross','Free Kick']
 @st.cache_data
 def load_data():
     data = pd.read_csv('shotsfor.csv')
@@ -94,7 +96,7 @@ if fora == 'For':
     p.insert(0,'All Players')
     player_name = st.selectbox('Select Player: ',options=p,placeholder='All Players')
     if player_name == 'All Players':
-        shotstype = st.selectbox('Select shot type: ',options = ['All shots','From Set Pieces'])
+        shotstype = st.selectbox('Select shot type: ',options = list(stype))
         if shotstype == 'All shots':
             col1, col2 = st.columns(2)
             with col1:
@@ -137,10 +139,10 @@ if fora == 'For':
                             yaxis_title='Number of Goals',
                             template='plotly_white')
                     st.plotly_chart(fig)
-        if shotstype != 'All shots':
+        if shotstype == 'From Corner':
             col1,col2 = st.columns(2)
             with col1:
-                    data = data[data['possession'].str.contains('Set piece|corner')].reset_index(drop='index')
+                    data = data[data['possession'].str.contains('Corner')].reset_index(drop='index')
                     mins = data.groupby('minute').size().reset_index()
                     mins.columns = ['minute','shots']
                     goals = data[data['goal']==True].reset_index()
@@ -179,6 +181,133 @@ if fora == 'For':
                             yaxis_title='Number of Goals',
                             template='plotly_white')
                     st.plotly_chart(fig)
+        if shotstype == 'Counter Attack':
+            col1,col2 = st.columns(2)
+            with col1:
+                    data = data[data['possession'].str.contains('Counterattack')].reset_index(drop='index')
+                    mins = data.groupby('minute').size().reset_index()
+                    mins.columns = ['minute','shots']
+                    goals = data[data['goal']==True].reset_index()
+                    goals = goals.groupby('minute').size().reset_index()
+                    goals.columns = ['minute','goals']
+                    fig = create_pitch_plot_all(data)
+                    fig.update_layout(title='Shot distribution')
+                    #final_fig.update_layout(width=4000)
+                    st.plotly_chart(fig, use_container_width=True)
+            with col2:
+                    bins = [0, 15, 30, 45, 60, 75, 90]
+                    labels = ["0-15", "15-30", "30-45", "45-60", "60-75", "75-90+"]
+                    mins['time_interval'] = pd.cut(mins['minute'], bins=bins, labels=labels, right=False, include_lowest=True)
+                    interval_data = mins.groupby('time_interval')['shots'].sum().reset_index()
+                    fig = go.Figure(data=[go.Bar(
+                        x=interval_data['time_interval'], 
+                        y=interval_data['shots'],
+                        marker_color='blue')])
+                    fig.update_layout(
+                            title='Distribution of Shots by Time Intervals',
+                            xaxis_title='Time Intervals (minutes)',
+                            yaxis_title='Number of Shots',
+                            template='plotly_white')
+                    st.plotly_chart(fig)
+                    bins = [0, 15, 30, 45, 60, 75, 90]
+                    labels = ["0-15", "15-30", "30-45", "45-60", "60-75", "75-90+"]
+                    goals['time_interval'] = pd.cut(goals['minute'], bins=bins, labels=labels, right=False, include_lowest=True)
+                    interval_data = goals.groupby('time_interval')['goals'].sum().reset_index()
+                    fig = go.Figure(data=[go.Bar(
+                        x=interval_data['time_interval'], 
+                        y=interval_data['goals'],
+                        marker_color='red')])
+                    fig.update_layout(
+                            title='Distribution of Goals by Time Intervals',
+                            xaxis_title='Time Intervals (minutes)',
+                            yaxis_title='Number of Goals',
+                            template='plotly_white')
+                    st.plotly_chart(fig)
+        if shotstype == 'Free Kick':
+            col1,col2 = st.columns(2)
+            with col1:
+                    data = data[data['possession'].str.contains('Free kick')].reset_index(drop='index')
+                    mins = data.groupby('minute').size().reset_index()
+                    mins.columns = ['minute','shots']
+                    goals = data[data['goal']==True].reset_index()
+                    goals = goals.groupby('minute').size().reset_index()
+                    goals.columns = ['minute','goals']
+                    fig = create_pitch_plot_all(data)
+                    fig.update_layout(title='Shot distribution')
+                    #final_fig.update_layout(width=4000)
+                    st.plotly_chart(fig, use_container_width=True)
+            with col2:
+                    bins = [0, 15, 30, 45, 60, 75, 90]
+                    labels = ["0-15", "15-30", "30-45", "45-60", "60-75", "75-90+"]
+                    mins['time_interval'] = pd.cut(mins['minute'], bins=bins, labels=labels, right=False, include_lowest=True)
+                    interval_data = mins.groupby('time_interval')['shots'].sum().reset_index()
+                    fig = go.Figure(data=[go.Bar(
+                        x=interval_data['time_interval'], 
+                        y=interval_data['shots'],
+                        marker_color='blue')])
+                    fig.update_layout(
+                            title='Distribution of Shots by Time Intervals',
+                            xaxis_title='Time Intervals (minutes)',
+                            yaxis_title='Number of Shots',
+                            template='plotly_white')
+                    st.plotly_chart(fig)
+                    bins = [0, 15, 30, 45, 60, 75, 90]
+                    labels = ["0-15", "15-30", "30-45", "45-60", "60-75", "75-90+"]
+                    goals['time_interval'] = pd.cut(goals['minute'], bins=bins, labels=labels, right=False, include_lowest=True)
+                    interval_data = goals.groupby('time_interval')['goals'].sum().reset_index()
+                    fig = go.Figure(data=[go.Bar(
+                        x=interval_data['time_interval'], 
+                        y=interval_data['goals'],
+                        marker_color='red')])
+                    fig.update_layout(
+                            title='Distribution of Goals by Time Intervals',
+                            xaxis_title='Time Intervals (minutes)',
+                            yaxis_title='Number of Goals',
+                            template='plotly_white')
+                    st.plotly_chart(fig)
+        if shotstype == 'From Cross':
+            col1,col2 = st.columns(2)
+            with col1:
+                    data = data[data['types'].str.contains('from_cross')].reset_index(drop='index')
+                    mins = data.groupby('minute').size().reset_index()
+                    mins.columns = ['minute','shots']
+                    goals = data[data['goal']==True].reset_index()
+                    goals = goals.groupby('minute').size().reset_index()
+                    goals.columns = ['minute','goals']
+                    fig = create_pitch_plot_all(data)
+                    fig.update_layout(title='Shot distribution')
+                    #final_fig.update_layout(width=4000)
+                    st.plotly_chart(fig, use_container_width=True)
+            with col2:
+                    bins = [0, 15, 30, 45, 60, 75, 90]
+                    labels = ["0-15", "15-30", "30-45", "45-60", "60-75", "75-90+"]
+                    mins['time_interval'] = pd.cut(mins['minute'], bins=bins, labels=labels, right=False, include_lowest=True)
+                    interval_data = mins.groupby('time_interval')['shots'].sum().reset_index()
+                    fig = go.Figure(data=[go.Bar(
+                        x=interval_data['time_interval'], 
+                        y=interval_data['shots'],
+                        marker_color='blue')])
+                    fig.update_layout(
+                            title='Distribution of Shots by Time Intervals',
+                            xaxis_title='Time Intervals (minutes)',
+                            yaxis_title='Number of Shots',
+                            template='plotly_white')
+                    st.plotly_chart(fig)
+                    bins = [0, 15, 30, 45, 60, 75, 90]
+                    labels = ["0-15", "15-30", "30-45", "45-60", "60-75", "75-90+"]
+                    goals['time_interval'] = pd.cut(goals['minute'], bins=bins, labels=labels, right=False, include_lowest=True)
+                    interval_data = goals.groupby('time_interval')['goals'].sum().reset_index()
+                    fig = go.Figure(data=[go.Bar(
+                        x=interval_data['time_interval'], 
+                        y=interval_data['goals'],
+                        marker_color='red')])
+                    fig.update_layout(
+                            title='Distribution of Goals by Time Intervals',
+                            xaxis_title='Time Intervals (minutes)',
+                            yaxis_title='Number of Goals',
+                            template='plotly_white')
+                    st.plotly_chart(fig)
+
 
     
     if player_name != 'All Players':
